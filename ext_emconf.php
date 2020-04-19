f3461b9b5b6bd94aa038842ba0bdc17f0a9c9083
Malte Riechmann <kontakt@malteriechmann.de>
2020-04-19 16:03:17 +0200

    [TASK] Support TYPO3 v10

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,17 +2,17 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 't3visuellverstehen',
-    'description' => 'Show a small icon in the Toolbar to provide support information.',
+    'description' => 'Show a small icon in the TYPO3 backend toolbar to provide support information.',
     'category' => 'be',
     'author' => 'visuellverstehen',
     'author_email' => 'kontakt@visuellverstehen.de',
     'author_company' => 'visuellverstehen',
     'state' => 'stable',
     'clearCacheOnLoad' => false,
-    'version' => '1.0.3',
+    'version' => '1.0.4',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
+            'typo3' => '8.7.0-10.4.99',
         ]
     ]
 ];
