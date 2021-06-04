3ffa3342b8542eaa9a79717087cdb0717b820024
Rune Piper <rune.piper@visuellverstehen.de>
2021-06-04 09:37:39 +0200

    Update constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'visuellverstehen',
     'state' => 'stable',
     'clearCacheOnLoad' => false,
-    'version' => '1.0.10',
+    'version' => '1.0.11',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-11.1.99',
+            'typo3' => '8.7.0-11.2.99',
         ]
     ]
 ];
