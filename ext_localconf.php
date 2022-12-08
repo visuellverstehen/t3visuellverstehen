83204e37fd868d95758be0a021fe88d23da568b8
Rune Piper <kontakt@runepiper.de>
2022-12-08 08:03:29 +0100

    feat: set default loginFootnote
    Previously we did it via the AdditionalConfiguration.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,3 +3,11 @@
 defined('TYPO3_MODE') or defined('TYPO3') or die();
 
 $GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][] = \VV\T3visuellverstehen\ToolbarItems\ContactToolbarItem::class;
+
+$GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive($GLOBALS['TYPO3_CONF_VARS'], [
+    'EXTENSIONS' => [
+        'backend' => [
+            'loginFootnote' => 'Bei Fragen: kontakt@visuellverstehen.de',
+        ],
+    ],
+]);
